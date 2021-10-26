88c2e97a5e7d46a8f54b74061df7d9362f272f2b
Rohit171994 <rohitkumarpoddar1917@gmail.com>
2021-10-26 14:28:36 +0530

    redirect issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "../src/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch ,Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Login from "./components/login/login";
 import SignUp from "./components/registartion/registration";
 import Dashboard from "./Dashboard/Dashboard";
@@ -8,7 +8,7 @@ import { isAutheticated } from "./api/auth";
 
 function App() {
 
-  const {data}=isAutheticated();
+  const data = isAutheticated();
 
   return (
     <Router>
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/" component={Login}></Route>
           <Route path="/register" component={SignUp}></Route>
           <Route path="/dashboard" component={Dashboard}></Route>
-          {data? <Redirect to="/dashboard" /> : <Redirect to="/"/> }
+          {data ? <Redirect to="/dashboard" /> : <Redirect to="/" />}
         </main>
       </Switch>
     </Router>
--- src/Dashboard/Profile.js
+++ src/Dashboard/Profile.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useHistory } from "react-router";
-import {signout,isAutheticated} from '../api/auth'
+import { signout, isAutheticated } from '../api/auth'
 
 const Profile = () => {
-   const history= useHistory()
-   const { data } = isAutheticated();
-   console.log(data)
-    const handleLogout=()=>{
-      signout()
-     history.push('/')
+    const history = useHistory()
+    const data = isAutheticated();
+    console.log(data)
+    const handleLogout = () => {
+        signout()
+        history.push('/')
     }
     return (
         <>
@@ -16,7 +16,7 @@ const Profile = () => {
                 <div><h2>Profile</h2></div>
                 <div style={{ width: '200px', height: '200px', borderRadius: '150px', border: '1px solid black', margin: 'auto', marginTop: '50px' }}><img scr="https://pbs.twimg.com/media/EYQBiFZWoAwiWOx.jpg" alt="" /></div>
                 <div style={{ marginTop: '20px', marginBottom: '10px' }}><h3>{data.name}</h3></div>
-                <div style={{ textAlign: 'left' }}><UserInfo  userInfo={data}/></div>
+                <div style={{ textAlign: 'left' }}><UserInfo userInfo={data} /></div>
                 <div><button
                     style={{
                         width: '230px', backgroundColor: "#795EBF",
