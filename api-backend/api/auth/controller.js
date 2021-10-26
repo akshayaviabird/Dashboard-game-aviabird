762c7b9252e55af0dd229a95354ca7b789c2979c
The-Fuse <rohitkumaryadav141002@gmail.com>
2021-10-26 10:41:57 +0530

    Logout enabled
    Logout logic in controller.js

--- api-backend/api/auth/controller.js
+++ api-backend/api/auth/controller.js
@@ -37,6 +37,18 @@ exports.login = async (req,res,next) => {
     sendTokenResponse(user,200,res);
 };
 
+exports.logout = async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    });
+};
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     // Create token
