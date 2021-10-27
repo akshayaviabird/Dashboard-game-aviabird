0c31547783f7f59f3704a6c34a90b70cd280452a
=Akshay <=akshay.m@aviabird.com>
2021-10-27 15:01:49 +0530

    playgames section

--- src/Dashboard/PlayGames.js
+++ src/Dashboard/PlayGames.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 const PlayGames = () => {
+    let game=[
+        {name:'Avia-Poker',discription:'play poker with your friends',hostURl:"https://aviapoker.herokuapp.com",imageurl:'https://aviapoker.herokuapp.com/css/card-game.jpg'}
+    ]
     return (
         <>
             <div className="row" style={{ margin: '1px' }}>
@@ -10,25 +13,27 @@ const PlayGames = () => {
                 <div className="col-sm-12"
                     style={{
                         marginTop: '10px',
+
                         overflowY: "scroll",
                         height: "80vh"
                     }}>
-                    {[1, 2, 3].map(x => <GameCard />)}
+                        {game.map((item)=><GameCard item={item}/>)} 
                 </div>
             </div>
         </>
     )
 }
 
-const GameCard = () => {
+const GameCard = ({item}) => {
+  
     return (
         <div className="row" style={{ marginTop: "20px" }}>
             <div className="col-sm-12">
                 <div style={{ background: '#35B6D0', margin: 'auto', width: '90%', display: 'flex', justifyContent: 'space-between', borderRadius: '10px', boxShadow: "0px 5px 0px 5px grey" }}>
                     <div style={{ paddingLeft: '10%', textAlign: 'left' }} >
-                        <h3 style={{ color: 'white' }}>Game Name</h3>
-                        <p>Game description</p>
-                      <a href="https://aviapoker.herokuapp.com" target="_blank" rel="noreferrer"> <button style={{
+                        <h3 style={{ color: 'white' }}>{item.name}</h3>
+                        <p>{item.discription}</p>
+                      <a href={item.hostURl}target="_blank" rel="noreferrer"> <button style={{
                             width: '187px', backgroundColor: "#795EBF",
                             color: "white",
                             fontSize: "1.5em",
@@ -39,7 +44,7 @@ const GameCard = () => {
                         }}>Play</button></a> 
                     </div>
                     <div style={{ width: '150px', height: '128px' }}>
-                        <img src="https://pbs.twimg.com/media/EYQBiFZWoAwiWOx.jpg" alt="" style={{ width: '100%', height: '100%', borderRadius: '0 10px 10px 0' }} />
+                        <img src={item.imageurl}alt="" style={{ width: '100%', height: '100%', borderRadius: '0 10px 10px 0' }} />
                     </div>
                 </div>
             </div>
